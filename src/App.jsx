64ffdeaef53eed658fd8386040b78bdc22a35d05
RedHorseAatman <smith@localhost.localdomain>
2023-12-25 17:46:48 +0530

    minor change with some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,37 @@
-import React from "react";
-import "./App.css";
-import Card from './card';
+import React from 'react';
+import './App.css';
+import './assets/rklogo.png';
 
 function App() {
   return (
     <>
-      <div className="div1">
-        <div className="div2">
-          <div className="sidebar">
-            <p>Courses</p>
-          </div>
-          <div className="side">
-            <p>DashBoard</p>
-          </div>
-          <div className="side">
-            <p>About</p>
-          </div>
-          <div className="side">
-            <p>calender</p>
-          </div>
-          <div className="side">
-            <p>Profile</p>
-          </div>
-        </div>
-
-        <div className="body">
-          <div className="navbar">
-            <div className="Icon">
-              <h1>DashBoard</h1>
-            </div>
-            <div className="settingIcon">
-              <h1>Setting</h1>
-            </div>
-          </div>
-          <div className="bodysync">
-            <Card
-              header="Puti herne subject"
-              subhead="subCode"
-              noti="N"
-              message="M"
-            />
-          </div>
-        </div>
-      </div>
+    <div className='fscreen'>
+    <div className='sidebar'>
+      <img src= "./src/assets/rklogo.png" />
+    <div className='sideitems'>
+      Account
+    </div>
+    <div className='sideitems'>
+      Dashboard
+    </div>
+    <div className='sideitems'>
+      courses
+    </div>
+    <div className='sideitems'>
+      calender
+    </div>
+    </div>
+    <div>
+    <div className='nbar'>
+    <div className='nitems'>
+    Dashboard
+    </div>
+      <img src='./src/assets/setting.png' />
+    </div>
+    </div>
+    </div>
     </>
-  );
+  )
 }
-export default App;
+
+export default App
